5fccd596c2542ff7fbcfa9867b970765f5a39d86
Hristo Dikov <hristodikov@yahoo.com>
2022-05-07 22:13:53 +0200

    New repo, couldn`t make the old one work, something went wrong with branching. Gotta read up on that.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Rock-Paper-Scissors
+<br />03.05.2022:
+Started the "Rock paper scissors" mini-game couple of months ago but didn't get very far. I just couldn't get it to play more than once and if it played more than once i just played with the same values so i decided to leave it be for the time being. I got to the Assignment where you need to add UI i decided to revisit and try to finish it so i can build from there.
+<br />Couldn't figure out where should i start and overall the code was really messy so i decided to scrape it and build from 0. The new code basically does what it is supposed to do. Most likely needs to be refined, but i'll leave it be for now. At some point i had to look for some help, because i wasn't sure i'm making any progress and it was getting kind of late but i wanted to finish before bed. 
+So here it is, my take on the classic Rock Paper Scissor. Soon with UI, hopefully!
+<br />07.05.2022:
+Finished the UI for the game. It's very basic and blocky. But it is still my first ever code with any kind of UI that i wrote, so i'm going to leave it like that for now, may come back later to shake things up.
---
+++ main.js
@@ -0,0 +1,115 @@
+// Implementing variables to count and show the score
+let humanScore = 0;
+let computerScore = 0;
+let roundResult;
+
+// Showing the winner of the round in the middle of the page
+const showRound = document.querySelector('#currentRound');
+const showCurrentRound = document.createElement('div');
+showRound.appendChild(showCurrentRound);
+
+// Showing the score at the bottom of the page
+const scoreBoard = document.querySelector('#scoring');
+const compScore = document.createElement('div');
+scoreBoard.appendChild(compScore);
+const humScore = document.createElement('div');
+scoreBoard.appendChild(humScore);
+
+// Implementing the possible choices in an array
+const choices = ['rock', 'paper', 'scissors'];
+    
+// Function for the computer turn using a random number
+function computerPlay() {
+    let randomChoice = Math.floor(Math.random()* choices.length);
+    return choices[randomChoice];
+}
+
+// Function that is going through the different possible plays
+function playOneRound(computer, human) {
+    
+    if ( computer === human ) {
+        roundResult = 'It`s a tie, no one wins this round! Continue!';
+        showCurrentRound.innerText = roundResult;
+        return roundResult;
+    }
+    else if ( computer === 'rock' ) {
+        if ( human === 'scissors' ) {
+            computerScore++;
+            compScore.innerText = `Computer : ${computerScore}`;
+            roundResult = 'Computer wins the round! Rock beats scissors!';
+            showCurrentRound.innerText = roundResult;
+            return roundResult;
+        } else if ( human === 'paper' ) {
+            humanScore++;
+            humScore.innerText = `Human : ${humanScore}`;
+            roundResult = 'Human wins the round! Paper beats rock!';
+            showCurrentRound.innerText = roundResult;
+            return roundResult;
+        }
+    }
+    else if ( computer === 'paper' ) {
+        if ( human === 'rock' ) {
+            computerScore++;
+            compScore.innerText = `Computer : ${computerScore}`;
+            roundResult = 'Computer wins the round! Paper beats rock!';
+            showCurrentRound.innerText = roundResult;
+            return roundResult;
+        } else if ( human === 'scissors' ){
+            humanScore++;
+            humScore.innerText = `Human : ${humanScore}`;
+            roundResult = 'Human wins the round! Scissors beats paper!';
+            showCurrentRound.innerText = roundResult;
+            return roundResult;
+        }
+    }
+    else if ( computer === 'scissors') {
+        if ( human === 'paper' ) {
+            computerScore++;
+            compScore.innerText = `Computer : ${computerScore}`;
+            roundResult = 'Computer wins the round! Scissors beats paper!';
+            showCurrentRound.innerText = roundResult;
+            return roundResult;
+        } else if ( human === 'rock' ){
+            humanScore++;
+            humScore.innerText = `Human : ${humanScore}`;
+            roundResult = 'Human wins the round! Rock beats scissors!';
+            showCurrentRound.innerText = roundResult;
+            return roundResult;
+        }
+    }
+}
+
+// A function that is going to refresh the page when the user type Y in the prompt
+function reload() {
+    const askUser = prompt('Would you like to restart the game? Type "y" to reload').toLocaleLowerCase();
+    if ( askUser === 'y' ) {
+        window.location.reload();
+    } else { 
+        throw "The game was ended by the user.";
+    }
+}
+
+// Making the buttons interactive and deciding how the program proceeds depending on the score
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        if ( ( computerScore < 5 ) && ( humanScore < 5 ) ) {
+            playOneRound(computerPlay(), button.id);
+        } else if ( computerScore === 5 ) {
+            // The final score will be shown and after 5 sec the user can chose to reload the game
+            showCurrentRound.innerText = 'The robots won! Humanity is doomed! Run for your lives!';
+            showCurrentRound.style.color = 'red';
+            setTimeout(reload, 5000);
+        } else if ( humanScore === 5 ) {
+            // The final score will be shown and after 5 sec the user can chose to reload the game
+            showCurrentRound.innerText = 'Human won! Humanity is saved! All go back to your lives!';
+            showCurrentRound.style.color = 'blue';
+            setTimeout(reload, 5000);
+        }
+    })
+})
+
+
+
+
+    //          (;..;)
